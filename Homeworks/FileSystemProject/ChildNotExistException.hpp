911a67f9a0a91b999d36142146993cce5a129a4a
MarioStoev99 <marcho00_99@abv.bg>
2021-03-06 15:04:02 +0200

    .

--- Homeworks/FileSystemProject/ChildNotExistException.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-class ChildNotExistException : public std::exception {
-public : 
-	const char* what() const {
-		return "Invalid directory or file's name";
-	}
-};
--- Homeworks/FileSystemProject/FileNotFoundException.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-class FileNotFoundException : public exception {
-	const char* what() const {
-		return "The file with this name doesn't exist in the system";
-	}
-};
